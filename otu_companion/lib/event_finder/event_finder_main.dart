2b7b9ab6f56ee8d05f1fcb159c6c46e8f50a24dc
Leon <leon.chow1@ontariotechu.net>
2020-10-18 18:18:14 -0400

    Add named routes in main, add events widget, add navigation for event and main

--- otu_companion/lib/event_finder/event_finder_main.dart
+++ otu_companion/lib/event_finder/event_finder_main.dart
@@ -1,6 +1,35 @@
 import 'package:flutter/material.dart';
 
+class Event {
+  String name;
+  String description;
+  DateTime startDateTime;
+  DateTime endDateTime; 
+
+  Event({this.name, this.description, this.startDateTime, this.endDateTime});
+
+  String toString() {
+    return '$name: ($startDateTime) ($endDateTime)';
+  }
+}
+
+class EventPageApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Event Finder Main',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
+
 class EventFinderMain extends StatefulWidget {
+  EventFinderMain({Key key, this.title}) : super(key: key);
+  
+  final String title;
+
   @override
   _EventFinderMainState createState() => _EventFinderMainState();
 }
@@ -8,8 +37,10 @@ class EventFinderMain extends StatefulWidget {
 class _EventFinderMainState extends State<EventFinderMain> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Hello from event_finder!")
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
     );
   }
 }
