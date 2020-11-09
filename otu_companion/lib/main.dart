4fc9d7f3d42c4ef7fd79aab8b0ff4b97b9e5c9a2
Leon <leon.chow1@ontariotechu.net>
2020-11-08 21:24:11 -0500

    Fix main.dart merge

--- otu_companion/lib/main.dart
+++ otu_companion/lib/main.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 
 import 'event_finder/event_finder_main.dart';
@@ -56,62 +55,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-=======
-import 'package:flutter/material.dart';
-
-import 'event_finder/event_finder_main.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'OTU Companion Hub',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'OTU Companion Hub'),
-      routes: <String, WidgetBuilder>{
-        '/eventFinderMain': (BuildContext context) =>
-          EventFinderMain(title: 'Event Finder'),
-      },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Event Finder'),
-          onPressed: () {
-            Navigator.pushNamed(
-              context,
-              '/eventFinderMain',
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
->>>>>>> event_finder
