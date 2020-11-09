dfdc439f6203815d1d5e22a61832789740317f2a
Kabilan <kabilan.manogaran@ontariotechu.net>
2020-11-09 18:02:00 -0500

    Part 1 of integration between master and event_finder branches

--- otu_companion/lib/main.dart
+++ otu_companion/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'event_finder/event_finder_main.dart';
-import 'package:otu_companion/event_finder/views/add_event.dart';
+import 'res/routes/routes.dart';
+import 'res/values/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,47 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'OTU Companion Hub',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'OTU Companion Hub'),
-      routes: <String, WidgetBuilder>{
-        '/eventFinderMain': (BuildContext context) =>
-            EventFinderMain(title: 'Event Finder'),
-        '/addEvent': (BuildContext context) =>
-            AddEventPage(title: 'Add an Event'),
-      },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Event Finder'),
-          onPressed: () {
-            Navigator.pushNamed(
-              context,
-              '/eventFinderMain',
-            );
-          },
-        ),
-      ),
+      theme: Themes.mainAppTheme(),
+      initialRoute: Routes.homeMain,
+      onGenerateRoute: Routes.generateRoute,
     );
   }
 }
---
+++ otu_companion/lib/res/routes/routes.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:otu_companion/event_finder/event_finder_main.dart';
+import 'package:otu_companion/event_finder/views/add_event.dart';
+import 'package:otu_companion/src/home_page/home_page_main.dart';
+
+class Routes {
+  // Login
+
+  // Home
+  static const homeMain = "/homeMain";
+  // Event Finder
+  static const eventFinderMain = "/eventFinderMain";
+  static const addEvent = "/addEvent";
+  // Classroom Finder
+
+  // Others
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case homeMain:
+        return MaterialPageRoute(
+            builder: (BuildContext context) =>
+                HomePageMain(title: 'Dash Board'));
+      case eventFinderMain:
+        return MaterialPageRoute(
+            builder: (BuildContext context) =>
+                EventFinderMain(title: 'Event Finder'));
+      case addEvent:
+        return MaterialPageRoute(
+            builder: (BuildContext context) =>
+                AddEventPage(title: 'Add Event'));
+    }
+
+    return null;
+  }
+}
