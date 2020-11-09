ff9e176edb6109f4dd568630ba3b8d7d57aca0e4
Kabilan <kabilan.manogaran@ontariotechu.net>
2020-11-08 21:48:02 -0500

    Added a date picker to add event form

--- otu_companion/lib/event_finder/views/add_event.dart
+++ otu_companion/lib/event_finder/views/add_event.dart
@@ -4,7 +4,7 @@ import '../model/event.dart';
 
 class AddEventPage extends StatefulWidget {
   AddEventPage({Key key, this.title}) : super(key: key);
-  
+
   final String title;
   @override
   _AddEventPageState createState() => _AddEventPageState();
@@ -15,9 +15,12 @@ class _AddEventPageState extends State<AddEventPage> {
 
   String _name = '';
   String _description = '';
-  
+  DateTime _eventDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
+    DateTime _currentDate = DateTime.now();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -26,6 +29,7 @@ class _AddEventPageState extends State<AddEventPage> {
         key: _formKey,
         child: Column(
           children: [
+            // Event Name Input
             TextFormField(
               decoration: const InputDecoration(
                 labelText: 'Event Name',
@@ -43,6 +47,7 @@ class _AddEventPageState extends State<AddEventPage> {
                 _name = newValue;
               },
             ),
+            // Decription Input
             TextFormField(
               decoration: const InputDecoration(
                 labelText: 'Description',
@@ -58,6 +63,44 @@ class _AddEventPageState extends State<AddEventPage> {
               onChanged: (String newValue) {
                 _description = newValue;
               },
+            ),
+            // Event Date input
+            Container(
+              child: Row(
+                children: [
+                  Text(
+                    "Event Date: ",
+                    style: TextStyle(color: Colors.grey[700], fontSize: 16.0),
+                  ),
+                  Text(_eventDate.day.toString() +
+                      "/" +
+                      _eventDate.month.toString() +
+                      "/" +
+                      _eventDate.year.toString()),
+                  FlatButton(
+                    child: Text("Select Date"),
+                    onPressed: () {
+                      showDatePicker(
+                              context: context,
+                              initialDate: _eventDate,
+                              firstDate: _currentDate,
+                              lastDate: DateTime(2150))
+                          .then((value) {
+                        setState(() {
+                          _eventDate = DateTime(
+                            value.year,
+                            value.month,
+                            value.day,
+                            _eventDate.hour,
+                            _eventDate.minute,
+                            _eventDate.second,
+                          );
+                        });
+                      });
+                    },
+                  ),
+                ],
+              ),
             ),
           ],
         ),
