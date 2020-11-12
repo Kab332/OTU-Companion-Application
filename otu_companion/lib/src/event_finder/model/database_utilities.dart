10ea70cd622fe6479ee60e0a2a2c59bbe330a05d
Leon <leon.chow1@ontariotechu.net>
2020-11-12 15:53:33 -0500

    Remove merge conflict text

--- otu_companion/lib/src/event_finder/model/database_utilities.dart
+++ otu_companion/lib/src/event_finder/model/database_utilities.dart
@@ -6,17 +6,12 @@ class DatabaseUtilities {
     var database = openDatabase(
       join(await getDatabasesPath(), 'event_views_data.db'),
       onCreate: (db, version) {
-<<<<<<< HEAD
         db.execute("CREATE TABLE event_views(id INTEGER PRIMARY KEY, viewType TEXT)");
         return db.insert(
           'event_views', // table name
           {'id': 1, 'viewType': "list"}, // Map values
           conflictAlgorithm: ConflictAlgorithm.replace // conflict algorithm if ID already exists in table
         );
-=======
-        db.execute(
-            "CREATE TABLE events(id INTEGER PRIMARY KEY, name TEXT, description TEXT, startDateTime TEXT, endDateTime TEXT)");
->>>>>>> fd10e51e84ff64e96dfe2974a3d4fe12b97b51c5
       },
       version: 1,
     );
