c185bde5e03874c0498442fc019f5dce66047cdc
Leon <leon.chow1@ontariotechu.net>
2020-11-12 17:06:06 -0500

    Add delete and insert view functions for future use

--- otu_companion/lib/src/event_finder/model/view_model.dart
+++ otu_companion/lib/src/event_finder/model/view_model.dart
@@ -6,7 +6,7 @@ import 'view.dart';
 
 // local storage for view model, to allow the user to change views in their event list
 class ViewModel {
-    // future function to get the views and returns it
+    // future function to get the views and returns it to the main event finder view
   Future<List<View>> getViews() async {
     final db = await DatabaseUtilities.init();
     final List<Map<String, dynamic>> data = await db.query('event_views');
@@ -28,6 +28,27 @@ class ViewModel {
       view.toMap(),
       where: 'id = ?',
       whereArgs: [view.id],
+      conflictAlgorithm: ConflictAlgorithm.replace
+    );
+  }
+
+  // future function to delete a view, takes a View as a parameter
+  Future<void> deleteView(View view) async {
+    print('updating ${view.id}');
+    final db = await DatabaseUtilities.init();
+    return db.delete(
+      'event_views',
+      where: 'id = ?',
+      whereArgs: [view.id],
+    );
+  }
+
+  // future function to insert a view, takes a View as a parameter
+  Future<void> insertView(View view) async {
+    final db = await DatabaseUtilities.init();
+    return db.insert(
+      'event_views',
+      view.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace
     );
   }
