5e598d090e535289f100cb6194c80c4d05d35dc4
Anthony Huang <Huang.Anthony@outlook.com>
2020-12-09 06:23:34 -0500

    Added Chat Model
    + Chat Model
    + Expanded Chat Page

--- otu_companion/lib/src/chat/chat_main.dart
+++ otu_companion/lib/src/chat/chat_main.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:otu_companion/src/navigation_views/main_side_drawer.dart';
 import 'package:otu_companion/src/services/firebase_database_service.dart';
 import 'package:otu_companion/res/routes/routes.dart';
+import 'package:otu_companion/src/chat/model/chat_rooms.dart';
+import 'package:otu_companion/src/chat/model/chat_model.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class ChatPage extends StatefulWidget {
   ChatPage({Key key, this.title}) : super(key: key);
@@ -13,8 +17,10 @@ class ChatPage extends StatefulWidget {
 }
 class _ChatPageState extends State<ChatPage>
 {
+  ChatModel _chatModel = ChatModel();
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
       drawer: MainSideDrawer(),
       resizeToAvoidBottomInset: false,
@@ -24,9 +30,63 @@ class _ChatPageState extends State<ChatPage>
       body: Container(
         child: Column(
           children: <Widget>[
-            _buildGlobalChatRoom(context),
-            Divider(
-              thickness: 2,
+            StreamBuilder(
+              stream: _chatModel.getUserAllRooms(),
+              builder: (context, snapshot)
+              {
+                if (snapshot.data == null)
+                {
+                  return Container();
+                }
+                else
+                {
+                  return ListView.builder(
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index)
+                    {
+                      DocumentReference documentReference = snapshot.data.documents[index];
+                      String roomName = "";
+                      String roomImageURL = "";
+
+                      documentReference.get().then(
+                      (value) => {
+                        setState((){
+                          roomName = value.data()["name"];
+                          roomImageURL = value.data()["imageURL"];
+                        })
+                      });
+
+                      return ListTile(
+                        title: Text(roomName),
+                        leading: CachedNetworkImage(
+                          imageUrl: roomImageURL,
+                          errorWidget: (context, url, error)
+                          {
+                            return CircleAvatar(
+                              backgroundColor: Colors.black,
+                              radius: 50,
+                            );
+                          },
+                          progressIndicatorBuilder: (context, url, downloadProgress)
+                          {
+                            return CircularProgressIndicator(value: downloadProgress.progress);
+                          },
+                          imageBuilder: (context, imageProvider)
+                          {
+                            return CircleAvatar(
+                              backgroundImage: imageProvider,
+                              radius: 50,
+                            );
+                          },
+                        ),
+                        onTap: (){
+                          Navigator.pushNamed(context, Routes.messagePage);
+                        },
+                      );
+                    }
+                  );
+                }
+              }
             ),
           ],
         ),
--- otu_companion/lib/src/chat/model/chat_model.dart
+++ otu_companion/lib/src/chat/model/chat_model.dart
@@ -1,26 +1,52 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:otu_companion/src/chat/model/chat_rooms.dart';
 
 class ChatModel
 {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  Future<List<ChatRooms>> getAllRooms() async {
-    var doc = await _db.collection('userChatRooms').doc(_firebaseAuth.currentUser.uid).get().then(
-      (DocumentSnapshot document) async{
-        List<Map<String, DocumentReference>> maps = document["people"];
-
-        maps.forEach((element) async{
-          DocumentSnapshot document = await _db.collection(element["otherUserID"].collection()).doc(element["otherUserID"].id).get();
-        });
-      }
-    );
-
-    doc.then((value){
-
-    })
+  Stream<QuerySnapshot> getUserAllRooms(){
+    return _db.collection('userData').doc(_firebaseAuth.currentUser.uid).collection("joinedChatRooms").snapshots();
   }
-  List<AccountInfo> _
+
+  Stream<QuerySnapshot> getRoomMessages(String roomID)
+  {
+    return _db.collection("chatRooms").doc(roomID).collection("messages").limit(20).snapshots();
+  }
+
+  Stream<QuerySnapshot> getRoomMessagesByReference(String roomID)
+  {
+    return _db.collection("chatRooms").doc(roomID).collection("messages").limit(20).snapshots();
+  }
+
+  Future<void> createNewUserRoom(ChatRooms chatRooms) async
+  {
+    DocumentReference documentReference = await _db.collection('chatRooms').doc(_firebaseAuth.currentUser.uid).collection("joinedChatRooms").add(chatRooms.toMap()).then(
+      (DocumentReference document) async{
+        document.collection("users").doc(_firebaseAuth.currentUser.uid).set({"roles" : "Owner"});
+        document.collection("messages").add({"message": "Hello!", "profileReference": _db.collection("profiles").doc(_firebaseAuth.currentUser.uid)});
+        return document;
+      });
+    _db.collection('userData').doc(_firebaseAuth.currentUser.uid).collection("joinedChatRooms").add({"roomID" : documentReference});
+  }
+
+  Future<void> insertUserRooms(DocumentReference documentReference) async
+  {
+    await _db.collection('userData').doc(_firebaseAuth.currentUser.uid).collection("joinedChatRooms").add({"roomID" : documentReference});
+    documentReference.collection("users").doc(_firebaseAuth.currentUser.uid).set({"roles" : "Member"});
+  }
+
+  Future<void> deleteRooms(DocumentReference documentReference) async
+  {
+    await documentReference.delete();
+  }
+
+  Future<void> updateChatRooms(DocumentReference documentReference, ChatRooms chatRooms) async
+  {
+    await documentReference.set(chatRooms.toMap());
+  }
 }
