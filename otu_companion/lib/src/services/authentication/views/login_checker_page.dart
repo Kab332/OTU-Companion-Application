46f29ec7943e3f6577ac350fdc329b93aa598a84
Leon <leon.chow1@ontariotechu.net>
2020-12-10 10:13:55 -0500

    change login_checker localization

--- otu_companion/lib/src/services/authentication/views/login_checker_page.dart
+++ otu_companion/lib/src/services/authentication/views/login_checker_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:otu_companion/src/home_page/home_page_main.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -12,7 +13,8 @@ class LoginCheckerPage extends StatelessWidget {
     // Verify User Signed in
     User user = Provider.of<User>(context);
     if (user != null) {
-      return HomePageMain(title: "Dash board",);
+      return HomePageMain(title: FlutterI18n.translate(
+            context, "routes.titles.dashboard"));
     }
     else {
       return LoginPage();
